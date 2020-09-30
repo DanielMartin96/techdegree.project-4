398085a93c4081c84f2824e44726c4dc4a5034ab
Daniel Martin <danielmartin1996@hotmail.com>
2020-09-30 14:51:59 +0100

    Updated comments

--- js/Phrase.js
+++ js/Phrase.js
@@ -6,4 +6,18 @@ class Phrase {
   constructor(phrase) {
     this.phrase = phrase.toLowerCase();
   }
+
+  /**
+   * Display phrase on game board
+   */
+  addPhraseToDisplay(phrase) {
+    // need to get phrase length
+    for (i = 0; i < phrase.length; i++) {
+      const li = document.createElement("li");
+      // get index of the phrase character
+      // console.log the value
+      // if statement to see if its a character or space
+      // set attribute relevant to the index of the word
+    }
+  }
 }
