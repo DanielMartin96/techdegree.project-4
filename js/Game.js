442e2b39eb18b8f78281cb918ed0f468b6edcfe8
Daniel Martin <danielmartin1996@hotmail.com>
2020-09-30 14:17:39 +0100

    14:17

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,24 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+  constructor() {
+    this.missed = 0;
+    this.phrases = createPhrases(5);
+    this.activePhrase = null;
+  }
+
+  /**
+   * Creates phrases for use in game
+   * @return {array} An array of phrases that could be used in the game
+   */
+  createPhrases(num) {
+    let phrases = [];
+    for (i = 0; i < num; i++) {
+      const phrase = "Hello world!";
+      phrases.push(phrase);
+    }
+    return phrases;
+  }
+}
